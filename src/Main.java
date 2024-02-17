48ed66a6331ba58e1166b5a201ba18445ca86d62
MahakPatel <mahakpatel0208@gmail.com>
2024-02-16 20:42:29 -0800

    modify Main.java file

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,14 @@ import com.hm1.aggregation.DriverForAggregation;
 import com.hm1.composition.DriverForComposition;
 
 
-
-
 public class Main {
     public static void main(String[] args) {
 
-//        DriverForInheritance driver = new DriverForInheritance();
-//        driver.print();
-//
-//        DriverForPolymorphism.run();
-      //  DriverForAggregation.run();
+        DriverForInheritance driver = new DriverForInheritance();
+        driver.print();
+
+        DriverForPolymorphism.run();
+        DriverForAggregation.run();
         DriverForComposition.run();
 
     }
