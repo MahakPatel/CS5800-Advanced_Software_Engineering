5aa5f6e7b6272d06d86f27f0313835969a8b3eed
MahakPatel <mahakpatel0208@gmail.com>
2024-02-16 18:49:19 -0800

    adding Main.java file

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import com.hm1.inheritance.DriverForInheritance;
+import com.hm1.polymorphism.DriverForPolymorphism;
+import com.hm1.aggregation.DriverForAggregation;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+//        DriverForInheritance driver = new DriverForInheritance();
+//        driver.print();
+//
+//        DriverForPolymorphism.run();
+        DriverForAggregation.run();
+
+    }
+}
