58835e0e9dd0a31c62fc0ae5e0b7976d5fd13161
MahakPatel <mahakpatel0208@gmail.com>
2024-05-17 00:19:59 -0700

    adding addCarbs and addProtein function in the MealBulder

--- src/com/Final/MealBuilder.java
+++ src/com/Final/MealBuilder.java
@@ -14,6 +14,17 @@ public class MealBuilder {
         return this;
     }
 
+    // Fluent interface to add carbs
+    public MealBuilder addCarbs(int carbs) {
+        this.carbs = carbs;
+        return this;
+    }
+
+    // Fluent interface to add protein
+    public MealBuilder addProtein(int protein) {
+        this.protein = protein;
+        return this;
+    }
 
     // Constructs and returns the final Meal object
     public Meal build() {
