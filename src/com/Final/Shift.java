4773136113c0cef0fca96c401c87a3074e1d4446
MahakPatel <mahakpatel0208@gmail.com>
2024-05-17 00:31:29 -0700

    adding isWithinShift function to check weather rider is in shift or not

--- src/com/Final/Shift.java
+++ src/com/Final/Shift.java
@@ -17,6 +17,17 @@ public enum Shift {
         return availability;
     }
 
-
+    public boolean isWithinShift(LocalTime time) {
+        switch (this) {
+            case FIRST_SHIFT:
+                return !time.isBefore(LocalTime.of(8, 0)) && !time.isAfter(LocalTime.of(16, 0));
+            case SECOND_SHIFT:
+                return !time.isBefore(LocalTime.of(16, 0)) && time.isBefore(LocalTime.of(23, 59)) || time.equals(LocalTime.of(0, 0));
+            case THIRD_SHIFT:
+                return time.isAfter(LocalTime.of(0, 0)) && !time.isAfter(LocalTime.of(8, 0));
+            default:
+                return false;
+        }
+    }
 }
 
