f85ef833ca798c4172122ca7ee3f1e823a233e0c
MahakPatel <mahakpatel0208@gmail.com>
2024-05-21 01:15:01 -0700

    modify order class

--- src/com/Final/Order.java
+++ src/com/Final/Order.java
@@ -1,37 +1,72 @@
 package com.Final;
+import java.util.*;
+class Order {
+    private Restaurant restaurant;
+    private Customer customer;
+    private String dietaryRestriction;
+    private List<FoodItem> foodItems;
+    private Driver driver;
+    private Date orderCreationTime;
+    private Date orderPickUpTime;
+    private Date orderDeliveredTime;
+    private List<OrderObserver> observers = new ArrayList<>();
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Order implements Subject {
-    private List<Observer> observers = new ArrayList<>();
-    private String status;
-
-    public Order() {
-        this.status = "Order Placed";
+    public Order(Restaurant restaurant, Customer customer, String dietaryRestriction, List<FoodItem> foodItems, Driver driver, Date orderCreationTime) {
+        this.restaurant = restaurant;
+        this.customer = customer;
+        this.dietaryRestriction = dietaryRestriction;
+        this.foodItems = foodItems;
+        this.driver = driver;
+        this.orderCreationTime = orderCreationTime;
     }
 
-    public void registerObserver(Observer o) {
-        observers.add(o);
-    }
-
-    public void removeObserver(Observer o) {
-        observers.remove(o);
+    public void addObserver(OrderObserver observer) {
+        observers.add(observer);
     }
 
     public void notifyObservers() {
-        for (Observer observer : observers) {
-            observer.update(status);
+        for (OrderObserver observer : observers) {
+            observer.update(this);
         }
     }
 
-    public void setStatus(String status) {
-        this.status = status;
-        notifyObservers();
+    public void setOrderPickUpTime(Date orderPickUpTime) {
+        this.orderPickUpTime = orderPickUpTime;
     }
 
-    public String getStatus() {
-        return status;
+    public void setOrderDeliveredTime(Date orderDeliveredTime) {
+        this.orderDeliveredTime = orderDeliveredTime;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public String getDietaryRestriction() {
+        return dietaryRestriction;
+    }
+
+    public List<FoodItem> getFoodItems() {
+        return foodItems;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public Date getOrderCreationTime() {
+        return orderCreationTime;
+    }
+
+    public Date getOrderPickUpTime() {
+        return orderPickUpTime;
+    }
+
+    public Date getOrderDeliveredTime() {
+        return orderDeliveredTime;
     }
 }
-
