aa2e4dae65d53ad13635c6b153a564f861063489
MahakPatel <mahakpatel0208@gmail.com>
2024-05-21 01:19:05 -0700

    Creating FoodItemDecorator abstarct class to getdescription and getcost

---
+++ src/com/Final/FoodItemDecorator.java
@@ -0,0 +1,19 @@
+package com.Final;
+
+abstract class FoodItemDecorator implements FoodItem {
+    protected FoodItem foodItem;
+
+    public FoodItemDecorator(FoodItem foodItem) {
+        this.foodItem = foodItem;
+    }
+
+    @Override
+    public String getDescription() {
+        return foodItem.getDescription();
+    }
+
+    @Override
+    public double getCost() {
+        return foodItem.getCost();
+    }
+}
